a7a610d3582dd6b4af1eb3d25e29289bdcaf578d
Vis Tacitus <vistacitus@confoederatio.org>
2026-08-18 03:34:27 +0000

    development commit

--- autorun.bat
+++ autorun.bat
@@ -1,7 +1,7 @@
 @echo off
 :loop
 cls
-echo Eoscala 1.2.1/Velkscala 0.7.1 - Historical Economic and Population Statistics.
+echo Eoscala 1.3/Velkscala 0.8 - Historical Economic and Population Statistics.
 node --max-old-space-size=128000 --expose-gc --trace-uncaught "main.js"
 pause
 goto loop
---
+++ input/stadester/stadester_urban_rasters/.rename_to_urbc.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for %%F in (stadester_urban_*.png) do (
+    set "filename=%%~nF"
+    
+    rem Extract the year (token 3)
+    for /f "tokens=3 delims=_" %%A in ("!filename!") do (
+        set "year=%%A"
+        
+        rem Check if the year is negative (which represents BC)
+        if "!year:~0,1!"=="-" (
+            set "abs_year=!year:~1!"
+            set "era=BC"
+        ) else (
+            set "abs_year=!year!"
+            set "era=AD"
+        )
+        
+        ren "%%F" "urbc_!abs_year!!era!_number.png"
+    )
+)
